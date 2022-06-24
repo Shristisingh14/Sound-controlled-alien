87ad3493e326a1ff5d5276dd590549e88a3ab75f
Shristisingh14 <84385618+Shristisingh14@users.noreply.github.com>
2022-06-24 16:58:37 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function startclassification() 
+{
+    navigator.mediaDevices.getUserMedia({ audio: true});
+    classifier = ml5.soundClassifier('https://teachablemachine.withgoogle.com/models/jtVr8Sn5Y/model.json', modelReady );
+}
+
+function modelReady() {
+    classifier.classify( gotResults);
+}
+
+function gotResults(error, results) {
+    if(error) {
+        console.log(error);
+    } else {
+        console.log(results);
+    }
+}
